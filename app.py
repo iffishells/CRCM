9b7c634a90b3bc56207723e54d38b5b39d811265
iffishells <iffishells@gmail.com>
2023-08-08 23:11:12 +0500

    added cleaned data and modified script

--- app.py
+++ app.py
@@ -23,42 +23,16 @@ def normalize_values(df,current_feature_name ,current_scaled_feature_name , new_
     
     return temp
 
-df = pd.read_csv('speed-dating_csv.csv') 
-df  =  df[df['has_null']==0]
-# df = normalize_values(df,'yoga','d_yoga','yoga_normalized')
-df = normalize_values(df,'shopping','d_shopping','shopping_normalized')
-# df = normalize_values(df,'concerts','d_concerts','concerts_normalized')
-df = normalize_values(df,'theater','d_theater','theater_normalized')
-df = normalize_values(df,'reading','d_reading','reading_normalized')
-
-df = normalize_values(df,'gaming','d_gaming','gaming_normalized')
-# df = normalize_values(df,'art','d_art','art_normalized')
-df = normalize_values(df,'dining','d_dining','dining_normalized')
-df = normalize_values(df,'music','d_music','music_normalized')
-df = normalize_values(df,'movies','d_movies','movies_normalized')
-df = normalize_values(df,'tv','d_tv','tv_normalized')
-df = normalize_values(df,'clubbing','d_clubbing','clubbing_normalized')
-df = normalize_values(df,'hiking','d_hiking','hiking_normalized')
-# df = normalize_values(df,'museums','d_museums','museums_normalized')
-df = normalize_values(df,'exercise','d_exercise','exercise_normalized')
-df = normalize_values(df,'sports','d_sports','sports_normalized')
-df = normalize_values(df,'tvsports','d_tvsports','tvsports_normalized')
-# df = normalize_values(df,'intelligence','d_intelligence','intelligence_normalized')
-# df = normalize_values(df,'sincere','d_sincere','sincere_normalized')
-# df = normalize_values(df,'ambition','d_ambition','ambition_normalized')
-df = normalize_values(df,'funny','d_funny','funny_normalized')
-# df = normalize_values(df,'attractive','d_attractive','attractive_normalized')
-
-
+df = pd.read_csv('CleadedSpeedDatingData.csv') 
 df.reset_index(inplace=True)
 df['compatbilityScore'] = 0 
 
-st.set_page_config(layout='wide',page_icon='!',page_title='Roommate Compatibility Test')
+st.set_page_config(layout='wide',page_icon='!',page_title='Roomate Compactibility Tests')
 path =  os.path.dirname(__file__)
 today = date.today()
 # Column 1
 # Title of the app
-st.title("Roomate Compactibility Test")
+st.title("Roomate Compactibility Tests")
 st.markdown("Use the options below to select your preferences for finding compatible roommates.")
 
 # Create two columns
@@ -72,7 +46,7 @@ with col1:
     st.markdown("<style>div[role='listbox'] { background-color: #f2f2f2; padding: 10px; }</style>", unsafe_allow_html=True)
     
     # Dropdown menu
-    selected_option_age = st.selectbox("Select an option for Age :", df['age'].unique().tolist())
+    selected_option_age = st.selectbox("Select an option for Age :", df['age-range'].unique().tolist())
     # Display the selected option
     st.write("You selected:", selected_option_age)
 
@@ -84,17 +58,10 @@ with col1:
     # Display the selected option
     st.write("You selected:", selected_option_race)
 
-    selected_option_field = st.selectbox("Select an option for Field of work :", df['field'].unique().tolist())
+    selected_option_field = st.selectbox("Select an option for Field :", df['field'].unique().tolist())
     # Display the selected option
     st.write("You selected:", selected_option_field)
 
-    # selected_option_yoga = st.selectbox("Select an option for Yoga(Preference order(0-1)) :", df['yoga_normalized'].unique().tolist())
-    # # Display the selected option
-    # st.write("You selected:", selected_option_yoga)
-    
-    # selected_option_art = st.selectbox("Select an option for Artist Personality(Preference order(0-1)) :", df['art_normalized'].unique().tolist())
-    # # Display the selected option
-    # st.write("You selected:", selected_option_art)
 
     selected_option_music = st.selectbox("Select an option for Music Listening Habit(Preference order(0-1)) :", df['music_normalized'].unique().tolist())
     # Display the selected option
@@ -104,41 +71,20 @@ with col1:
     # Display the selected option
     st.write("You selected:", selected_option_dining)
     
-    # selected_option_meseums = st.selectbox("Select an option for Museums Habit(Preference order(0-1)) :", df['museums_normalized'].unique().tolist())
-    # # Display the selected option
-    # st.write("You selected:", selected_option_meseums)
+
 
    
 
     # # Add a line break
     # st.markdown("<br>", unsafe_allow_html=True)
 with col2:
-        
-    # selected_option_shopping  = st.selectbox("Select an option for Shopping Habit(Preference order(0-1)) :", df['shopping_normalized'].unique().tolist())
-    # # Display the selected option
-    # st.write("You selected:", selected_option_shopping)
-
-
-    # selected_option_concert = st.selectbox("Select an option for Concerts Habit(Preference order(0-1)) :", df['concerts_normalized'].unique().tolist())
-    # # Display the selected option
-    # st.write("You selected:", selected_option_concert)
-
-
-    selected_option_theater = st.selectbox("Select an option for Theater Habit(Preference order(0-1)) :", df['theater_normalized'].unique().tolist())
-    # Display the selected option
-    st.write("You selected:", selected_option_theater)
-
-
     selected_option_reading = st.selectbox("Select an option for Reading Habit(Preference order(0-1)) :", df['reading_normalized'].unique().tolist())
     # Display the selected option
     st.write("You selected:", selected_option_reading)
 
-
     selected_option_gaming = st.selectbox("Select an option for Gaming Habit(Preference order(0-1)) :", df['gaming_normalized'].unique().tolist())
     # Display the selected option
     st.write("You selected:", selected_option_gaming)
-
-
 
     selected_option_movie = st.selectbox("Select an option for Watching Movie(Preference order(0-1)) :", df['movies_normalized'].unique().tolist())
     # Display the selected option
@@ -147,30 +93,14 @@ with col2:
     selected_option_sports = st.selectbox("Select an option for Sports Habit(Preference order(0-1)) :", df['sports_normalized'].unique().tolist())
     # Display the selected option
     st.write("You selected:", selected_option_sports)
+    selected_option_food = st.selectbox("Select an option for Sports Habit(Preference order(0-1)) :", df['FoodChoice'].unique().tolist())
+    # Display the selected option
+    st.write("You selected:", selected_option_food)
 
-    # selected_option_intelligence = st.selectbox("Select an option for Intelligence Level(Preference order(0-1)) :", df['intelligence_normalized'].unique().tolist())
-    # # Display the selected option
-    # st.write("You selected:", selected_option_intelligence)
-
-    # selected_option_sincere = st.selectbox("Select an option for Sincere Level(Preference order(0-1)) :", df['sincere_normalized'].unique().tolist())
-    # # Display the selected option
-    # st.write("You selected:", selected_option_sincere)
 
   
 
 with col3:
-    
-    # selected_option_ambition = st.selectbox("Select an option for Ambition Level(Preference order(0-1)) :", df['ambition_normalized'].unique().tolist())
-    # # Display the selected option
-    # st.write("You selected:", selected_option_ambition)
-
-    selected_option_funny = st.selectbox("Select an option for Funny Personality Level(Preference order(0-1)) :", df['funny_normalized'].unique().tolist())
-    # Display the selected option
-    st.write("You selected:", selected_option_funny)
-
-    # selected_option_attractive = st.selectbox("Select an option for Attractive Personality Level(Preference order(0-1)) :", df['attractive_normalized'].unique().tolist())
-    # # Display the selected option
-    # st.write("You selected:", selected_option_attractive)
     
     selected_option_tv = st.selectbox("Select an option for Watching TV (Preference order(0-1)) :", df['tv_normalized'].unique().tolist())
     # Display the selected option
@@ -188,35 +118,30 @@ with col3:
     # Display the selected option
     st.write("You selected:", selected_option_exercise)
 
+    selected_option_religion = st.selectbox("Select an option for Importance of Same Religion(0-1)) :", df['importance_same_religion_normalized'].unique().tolist())
+    # Display the selected option
+    st.write("You selected:", selected_option_religion)
+
   
 
 
 
-user_preferences = {'age': int(selected_option_age),
+user_preferences = {'age-range': selected_option_age,
                 'gender': selected_option_gender,
                 'race': selected_option_race,
                 'field': selected_option_field,
-                # 'yoga_normalized':selected_option_yoga,
-                # 'shopping_normalized':selected_option_shopping,
-                # 'concerts_normalized':selected_option_concert,
-                'theater_normalized':selected_option_theater,
                 'reading_normalized':selected_option_reading,
                 'gaming_normalized':selected_option_gaming,
-                # 'art_normalized':selected_option_art,
                 'music_normalized':selected_option_music,
                 'dining_normalized':selected_option_dining,
                 'movies_normalized':selected_option_movie,
                 'tv_normalized':selected_option_tv,
                 'clubbing_normalized':selected_option_clubbing,
                 'hiking_normalized':selected_option_hiking,
-                # 'museums_normalized':selected_option_meseums,
+
                 'exercise_normalized':selected_option_exercise,
                 'sports_normalized':selected_option_sports,
-                # 'intelligence_normalized':selected_option_intelligence,
-                # 'sincere_normalized':selected_option_sincere,
-                # 'ambition_normalized':selected_option_ambition,
-                'funny_normalized':selected_option_funny,
-                # 'attractive_normalized':selected_option_attractive,
+                'importance_same_religion_normalized':selected_option_religion
             }
 
 interested_records = []
@@ -229,20 +154,36 @@ for index , row in df.iterrows():
             interested_records.append(index)
             
             
-# interestedDf =  df.iloc[interested_records][['age','gender','race','field','yoga_normalized','shopping_normalized','concerts_normalized','compatbilityScore']]
+            
+                     # <img src="path_to_signal_image.png" alt="Signal Image" width="300">
 
-
-compatible_df = df[df['compatbilityScore'] != 0][['age','gender','race','field','compatbilityScore']]
+# Calculate the maximum compatibility score in the dataset
+max_compatibility_score = df['compatbilityScore'].max()
+# Iterate through the DataFrame and calculate the normalized compatibility score as a percentage
+for index, row in df.iterrows():
+    df.loc[index, 'compatbilityScore'] = np.round(int((row['compatbilityScore'] / max_compatibility_score) * 100),2)
+               
+compatible_df = df[df['compatbilityScore'] != 0][['age-range','gender','race','field','compatbilityScore']]
 compatible_sorted_df = compatible_df.sort_values(by='compatbilityScore', ascending=False)
-st.title("Interested Records - Sorted by Compatibility Score")
-styled_sorted_df = compatible_sorted_df.style.set_properties(**{'background-color': 'white', 'color': 'black'})
-st.dataframe(styled_sorted_df)
-
-
-# # Combine the interested records into a DataFrame
-# interested_df = pd.DataFrame(interested_records)
-
-# Display the interested records table
-# st.title("Interested Records")
-# # st.dataframe(interestedDf)
-# st.table(compatible_sorted_df)
+# st.title("Available Roommates - Sorted by Compatibility Score")
+# styled_sorted_df = compatible_sorted_df.style.set_properties(**{'background-color': 'white', 'color': 'black'})
+# st.dataframe(styled_sorted_df)
+col1,col2 = st.columns(2)
+with col1:
+    
+    compatible_df = compatible_df.head()
+    for index,row in compatible_df.iterrows():
+        st.markdown(
+            f"""
+            <div style="background-color: #f9f9f9; padding: 15px; border-radius: 15px; box-shadow: 2px 2px 5px #888888;">
+                <h3>Name- XYZ </h3>
+                <p><strong>Age Range :</strong> {row['age-range']}</p>
+                <p><strong>Gender :</strong> {row['gender']}</p>
+                <p><strong>Race :</strong> {row['race']}</p>
+                <p><strong>field of work :</strong> {row['field']}</p>
+                <p><strong>compatbilityScore:</strong>{row['compatbilityScore']} </p>
+                <p><strong>Description:</strong> This is a sample signal with some information.</p>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
